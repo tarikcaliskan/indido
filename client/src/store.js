4664a7138975619f1a75fcd8703e6f3817af0e4b
Tarık <atarikcaliskan@gmail.com>
2020-03-16 00:09:35 +0300

    blank page problem fixed

--- client/src/store.js
+++ client/src/store.js
@@ -5,14 +5,11 @@ import rootReducer from './reducers';
 const initialState = {};
 
 const middleware = [thunk];
-
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 const store = createStore(
 	rootReducer,
 	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
